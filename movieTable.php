4b72ff16dff864ec49c7b8c9826e3548da9719a7
Chang Chen Ong <twt6m@hotmail.com>
2019-01-04 16:10:52 +0000

    Added a SimpleXML demonstration to generate a table

---
+++ movieTable.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Movie Database</title>
+<meta charset='utf-8' />
+<body>
+<h2>My Movie Database</h2>
+<table border="2">
+	<tr bgcolor=" #54AAFF">
+	<td>Name</td>
+	<td>Category</td>
+	<td>Description</td>
+	<td>Director</td>
+	<td>Release Date</td>
+	</tr>
+	<tbody>
+	<?php
+	$movies = simplexml_load_file('movieXMLdefault.xml') or die("Error loading file!");
+	foreach($movies->children() as $dvd)
+	{
+		echo '<tr>';
+		echo '<td>'.$dvd->title.'</td>';
+		echo '<td>'.$dvd['category'].'</td>';
+		echo '<td>'.$dvd->description.'</td>';
+		echo '<td>'.$dvd->director.'</td>';
+		echo '<td>'.$dvd->releaseDate.'</td>';
+		echo '</tr>';
+	}
+	?>
+	</tbody>
+</table>
+</body>
+</head>
